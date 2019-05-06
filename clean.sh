2a8804ccd90ba7bb9a44553546e7553e7fc8f020
Gregoire Mahe <gregoire.mahe@corp.ovh.com>
2019-05-06 10:40:00 +0200

    add designate host unspawn

--- clean.sh
+++ clean.sh
@@ -7,7 +7,7 @@ for line in $(openstack server list -f value -c ID -c Name) ; do
     id=$(echo $line | awk '{ print $1}')
     name=$(echo $line | awk '{ print $2}')
 
-    if [[ "$name" =~ deployer|rabbit|mysql|keystone|nova|glance|horizon|neutron|compute ]]; then
+    if [[ "$name" =~ deployer|rabbit|mysql|keystone|nova|glance|horizon|neutron|compute|designate ]]; then
         echo "Deleting $name"
         todelete="$todelete $id"
     fi
